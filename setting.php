2652885a45b69c3f521c82fb4c4d2382e7e74e2f
caz7L <wahyupolsa@gmail.com>
2019-05-19 03:49:05 +0700

    new up

---
+++ setting.php
@@ -0,0 +1,227 @@
+<?php
+/* setting bot */
+$ads=$ijo."    [ official chanel caz7L ]".$t.$red."\t >> subcribe chanel << ".$t.$pth."[ ".$ijo."AW 2000".$pth." ]".$t;
+
+$donasi=false;
+
+$warn=$red."[!] warning !!!
+".$kn."[-] ini adalah program ilegal
+".$kn."[-] resiko sepenuhnya di tanggung pengguna".$t;
+
+$key="caz7L";
+$link_yogo="https://www.youtube.com/channel/UCrNm9Nd0WWzvwL2MfMal6eg";
+
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+recode:$ijo caz7L$putih |
+chanel :$kuning caz7L$putih |".$t;
+$msg_baca= "[•] script direcode oleh cazz".$t."[•] update skrip fix error 14/02/2019";
+$stat_baca=true;
+ 
+$ccu= $red."
+──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
+──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
+──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
+recode: ".$ijo."caz7L ".$pth."|| chanel: ".$ijo."caz7L".$t;
+$msg_ccu=$ijo."[•] script direcode oleh cazz";
+$stat_ccu=true;
+
+$mc=$br."
+──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀────
+──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
+──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
+".$pth."recode: ".$ijo."caz7L ".$pth."|| ".$ijo."subcribe chanel caz7L"
+.$pth."support: ".$ijo."subcribe chanel caz7L".$t;
+$msg_mc=$ijo."[•] This script doesn't work".$t."[•] skrip coid";
+$stat_mc=true;
+
+$flash="$red
+ █▀──── █────── ▄▀▄──── ▄▀▀──── █────── ▄▀▀───── ▄▀▄
+ █▀──── █─▄──── █▀█──── ─▀▄──── █▀▄──── █─▀▌──── █─█
+ ▀───── ▀▀▀──── ▀─▀──── ▀▀───── ▀─▀──── ▀▀▀───── ─▀─
+$putih  recode :$ijo caz7L$putih ||||| chanel :$turkis caz7L".$t."$putih  support :$ijo caz7L$putih ||||| chanel :$turkis caz7L";
+$msg_flash=$turkis."[•] flash go";
+$stat_flash=true;
+
+$yogov= $kuning."
+ ▀▄─▄▀ ▄▀▄ ▄▀▀─ ▄▀▄──── ▐▌─▐▌ ▀ █▀▄ █▀▀ ▄▀▄
+ ──█── █─█ █─▀▌ █─█──── ─▀▄▀─ █ █─█ █▀▀ █─█
+ ──▀── ─▀─ ▀▀▀─ ─▀───── ──▀── ▀ ▀▀─ ▀▀▀ ─▀─".$t.
+$ijo."[ v 1.0 ] ".$putih."recode: ".$ijo."caz7L".$t.">> subcribe chanel caz7L".$t.">> subcribe chanel caz7L".$t;
+$msg_yogo=$ijo."[•] enjoy your life";
+$stat_yogo=true;
+$yogo=["tes"];
+
+$wot=$pth."
+    ___ Wheel __       __O_f__        _Trust__".$red."
+    __ |     / / ".$kn."      __  __ \ ".$ijo."      ___  __/".$red."
+    __ | /| / /  ".$kn."      _  / / / ".$ijo."      __  /   ".$red."
+    __ |/ |/ /  ".$kn."       / /_/ /  ".$ijo."      _  /    ".$red."
+    ____/|__/   ".$kn."       \____/   ".$ijo."      /_/     ".$pth."
+========[ v 1.0 ][recode: caz7L]========".$t;
+$stat_wot=false;
+$msg_wot=$ijo."[•] skrip dalam perbaikan";
+
+
+$emon=$ijo."
+──── █▀▀──── █▄─▄█──── ▄▀▄──── █▄─█────
+──── █▀▀──── █─█─█──── █─█──── █─▀█────
+──── ▀▀▀──── ▀───▀──── ─▀───── ▀──▀────".$pth."
+==[recode:".$ijo." caz7L".$pth."]=|=".$tr."[ v : 1 . 0 ]".$pth."==".$t;
+$stat_emon=true;
+$auto_ref=true;
+$msg_emon= $ijo."[•] enjoy your life";
+
+$you_me=$red."
+──── ▀▄─▄▀ ▄▀▄ █─█──── █▄─▄█ █▀▀────
+──── ──█── █─█ █─█──── █─█─█ █▀▀────
+──── ──▀── ─▀─ ─▀───── ▀───▀ ▀▀▀────".$t.$pth."========".$ijo."[recode : caz7L]".$pth."========".$t;
+$youme=true;
+$msg_you=$ijo."[•] enjoy your life";
+
+$ltc=$red."
+_______________________________________
+    _| [".$ijo."ROOT".$red."]  _|  ".$ijo." |||||||||||||||||||".$red."                   
+    _|       _|_|_|_|       _|_|_|      
+    _|         _|         _|            
+    _|  __     _|         _|         
+_____|____|__".$ijo."I".$red."__|_|___".$ijo."E".$red."____|_|_|_|_".$ijo."OIN_".$pth."
+[ v.1.0 ]========[ recode : caz7L ] ".$t;
+$msg_ltc="[•] enjoy your life";
+$do_ltc=false;
+$stat_ltc=true;
+
+$eth_far=$tr."
+──── █▀▀ ▀█▀ █────── █▀ ▄▀▄ █▀▀▄ █▄─▄█ █▀▀ █▀▀▄────
+──── █▀▀ ─█─ █▀▄──── █▀ █▀█ █▐█▀ █─█─█ █▀▀ █▐█▀────
+──── ▀▀▀ ─▀─ ▀─▀──── ▀─ ▀─▀ ▀─▀▀ ▀───▀ ▀▀▀ ▀─▀▀────
+======[".$ijo." version 1.0".$tr." ]===[ recode:".$ijo." caz7L".$tr." ]======".$t;
+$msg_eth_far=$ijo."[•] enjoy your life";
+$stat_eth_far=true;
+
+$ice=$br."
+──── ▀ ▄▀ █▀▀──── █▀▄ █▀▀▄ █▀▀ ▄▀▄ █─▄▀ █▀▀ █▀▀▄────
+──── █ █─ █▀▀──── █▀█ █▐█▀ █▀▀ █▀█ █▀▄─ █▀▀ █▐█▀────
+──── ▀ ─▀ ▀▀▀──── ▀▀─ ▀─▀▀ ▀▀▀ ▀─▀ ▀─▀▀ ▀▀▀ ▀─▀▀────".$pth."
+|||=============[".$ijo." recode: caz7L".$pth." ]=============|||".$t;
+$msg_ice=$ijo."[•]=[info]".$t.
+"[-] update lagi icebreaker 13 maret tutor chanel ytb caz7L".$t;
+
+$stat_ice=true;
+
+$epi=$kn."
+ █▀▀──── █▀▄──── ▀──── ▄▀──── ▄▀▄──── ▄▀▀──── █──
+ █▀▀──── █─█──── █──── █───── █▀█──── ─▀▄──── █▀▄
+ ▀▀▀──── █▀───── ▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀
+ ".$pth."[ ".$tr."v 1.0".$pth." ]==[ ".$tr."recode:".$ijo." caz7L".$pth." ]".$t;
+$msg_epi=$ijo."[•] update skrip https://github.com/caz7L/epicash".$t;
+$stat_epi1=true;
+
+$cash= $br."
+ ▄▀ ▄▀▄ ▄▀▀ █────── █───█ ▄▀▄ █── █── █▀▀ ▀█▀
+ █─ █▀█ ─▀▄ █▀▄──── █─█─█ █▀█ █─▄ █─▄ █▀▀ ─█─
+ ─▀ ▀─▀ ▀▀─ ▀─▀──── ─▀─▀─ ▀─▀ ▀▀▀ ▀▀▀ ▀▀▀ ─▀─
+====[".$ijo." version 1.0".$br." ]=[ recode:".$ijo." caz7L".$br." ]====".$t;
+$msg_cash=$ijo."[•] enjoy your life";
+$stat_cash=true;
+
+
+$spbu=$pth."
+ ==[ s  p  i  n  n  i  n  g   b  u  c  k  s ]==".$red."
+ _______".$kn."       _____ ".$ijo."      ______ ".$br."      _     _".$red."
+ |______".$kn."      |_____]".$ijo."      |_____]".$br."      |     |".$red."
+ ______|".$kn."      |      ".$ijo."      |_____]".$br."      |_____|".$pth."
+ ======[ ".$tr."v 1.0".$pth." ]====[ ".$tr."recode: ".$br."caz7L".$pth." ]======".$t;
+$msg_spbu=$ijo."[•] enjoy your life";
+$stat_spbu=true;
+
+$cbn=$tr."                                                          
+[v 1.0]".$red."_|_|_|".$pth."recode".$red."_|_|_|".$ijo." caz7L".$red."_|".$kn." C B N".$red."_|              
+     _|             _|    _|       _|_|    _|              
+     _|             _|_|_|         _|  _|  _|              
+     _|             _|    _|       _|    _|_|              
+     _|_|_|         _|_|_|         _|      _|".$br."           
+_[ T h e ]__".$kn."[ B i t c o i n ]__".$ijo."[ W h e e l ]_".$t;
+$msg_cbn=$ijo."[•] enjoy your life";
+$stat_cbn=true;
+
+$newstom= $br."
+ █▄─█ █▀▀ █───█ ▄▀▀──── ▀█▀ ▄▀▄ █▄─▄█
+ █─▀█ █▀▀ █─█─█ ─▀▄──── ─█─ █─█ █─█─█
+ ▀──▀ ▀▀▀ ─▀─▀─ ▀▀───── ─▀─ ─▀─ ▀───▀".$pth."
+    [v 1.0 ]=[".$tr." recode: ".$ijo."caz7L".$pth." ]".$t.$tr;
+$msg_tom=$ijo."[•] enjoy your life";
+$stat_tom=true;
+
+$glu=$ijo."
+____ _    _  _ _ _ _ ____ ____ ___  ____    
+| __ |    |  | | | | |__| |__/ |  \ [__     
+|__] |___ |__| |_|_| |  | |  \ |__/ ___]".$pth."    
+----------------------------------------".$tr."
+[===[ ".$pth."v 1.0".$tr." ]==[".$pth." recode:".$ijo." caz7L".$tr." ]===]".$pth."
+----------------------------------------".$t;
+$msg_glu=$ijo."[•] enjoy your life";
+$stat_glu=true;
+
+$yogop= $kuning."
+ ▀▄─▄▀ ▄▀▄ ▄▀▀─ ▄▀▄──── ▐▌─▐▌ ▀ █▀▄ █▀▀ ▄▀▄
+ ──█── █─█ █─▀▌ █─█──── ─▀▄▀─ █ █─█ █▀▀ █─█
+ ──▀── ─▀─ ▀▀▀─ ─▀───── ──▀── ▀ ▀▀─ ▀▀▀ ─▀─".$t.$ijo."[ v ".$v." ] ".$putih."recode: ".$ijo."caz7L ".$pth."|| mod: ".$ijo."caz7L".$t;
+$msg_yogop=$ijo."[•] info : skrip ini di kelola oleh caz7L";
+$stat_yogop=true;
+
+$ecash=$kn."
+──── █▀▀──── ▄▀──── ▄▀▄──── ▄▀▀──── █──────
+──── █▀▀──── █───── █▀█──── ─▀▄──── █▀▄────
+──── ▀▀▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀────".$pth."
+[ recode [".$ijo."caz7L".$pth."]=[ chanel [caz7L]".$pth.$t;
+$msg_ecash=$ijo."[•] enjoy your life";
+$stat_ecash=true;
+
+$panda=$ijo."
+──── ▄▀ ▄▀▄ ▄▀▀ █──────".$kn." █▀▄ ▄▀▄ █▄─█ █▀▄ ▄▀▄────".$ijo."
+──── █─ █▀█ ─▀▄ █▀▄────".$kn." █─█ █▀█ █─▀█ █─█ █▀█────".$ijo."
+──── ─▀ ▀─▀ ▀▀─ ▀─▀────".$kn." █▀─ ▀─▀ ▀──▀ ▀▀─ ▀─▀────".$pth."
+=[ recode: ".$tr."caz7L".$pth." ]=[ team: ".$br."Black".$red."Coder".$ijo."Crush".$pth."]=".$t;
+$msg_panda="[•]=[ enjoy your life ]";
+$stat_panda=true;
+
+$big=$br."
+──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
+──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
+──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
+=| recode : ".$ijo."caz7L ".$pth."|==| versi :".$tr." [ 1.0 ]".$pth." |=".$t;
+$msg_big="[•]=[ enjoy your life ]";
+$stat_big=true;
+
+$buzz=$ijo."
+──── █▀▄ █─█ ▀▀▀▀█ ▀▀▀▀█ █▀▄ █▀▀▄ █▀▀ ▄▀▄ █─▄▀────".$kn."
+──── █▀█ █─█ ─▄▀▀─ ─▄▀▀─ █▀█ █▐█▀ █▀▀ █▀█ █▀▄─────".$red."
+──── ▀▀─ ─▀─ ▀▀▀▀▀ ▀▀▀▀▀ ▀▀─ ▀─▀▀ ▀▀▀ ▀─▀ ▀─▀▀────".$pth."
+||| recode : ".$ijo."caz7L ".$pth."|||".$br." chanel : ".$tr."caz7L ".$pth."|||
+--------------------------------------------------".$t;
+$msg_buzz="[•]=[ enjoy your life ]";
+$stat_buzz=true;
+
+$yodo=$kn."
+──── ▀▄─▄▀──── ▄▀▄──── █▀▄──── ▄▀▄────
+──── ──█────── █─█──── █─█──── █─█────
+──── ──▀────── ─▀───── ▀▀───── ─▀─────".$pth."
+[ crator :".$ijo." caz7L".$pth." ]=[ versi :".$ijo." $info ]".$t;
+$msg_yodo=$ijo."[ update skrip v 1.1 add otp all country ]";
+$stat_yodo=true;
+
+$qriket=$ijo."
+_______ ________ ______________ ____________________
+__  __ \___  __ \____  _/___  //_/___  ____/___  __/
+_  / / /__  /_/ / __  /  __  ,<   __  __/   __  /   $kn
+/ /_/ / _  _, _/ __/ /   _  /| |  _  /___   _  /    
+\___\_\ /_/ |_|  /___/   /_/ |_|  /_____/   /_/     $pth
+                                                    
+[ versi:$ijo $vr $pth] [ recode :$ijo c a z z ] ".$t;
+$msg_qr=$ijo."[ enjoy your life ]";
+$stat_qr=true;
+
+?>
---
+++ user.php
@@ -0,0 +1,11 @@
+<?php
+$stat_sseth=true;
+$msgsseth= "[---] enjoy your life";
+$stat_cp = true;
+$msgcp = "[--] enjoy your life";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."recode:".$ijo." caz7L   ".$putih."[--]  ide  :".$ijo." caz channel  ".$t;
